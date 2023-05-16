9057092e11ea45febeb3c113081d34d35a4a6244
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-05-16 12:02:31 -0300

    ADD: fuctional API key call

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
+import axios from "axios";
+
+const apiKey = import.meta.env.VITE_API_KEY as string;
+
+function getStatus() {
+  var config = {
+    method: "get",
+    url: "https://v3.football.api-sports.io/status",
+    headers: {
+      "x-rapidapi-key": apiKey,
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <button onClick={getStatus}>Get Status</button>
+    </div>
+  );
 }
